ee13b0cd288563ef17e41f32aa02db11790f96e9
Derek Wuelfrath <dwuelfrath@inverse.ca>
2014-12-05 15:07:24 -0500

    Keep track of machine account in case of machine auth

--- db/upgrade-4.5.0-4.6.0.sql
+++ db/upgrade-4.5.0-4.6.0.sql
@@ -9,3 +9,9 @@ ALTER TABLE locationlog_history
     ADD `stripped_user_name` varchar(255) DEFAULT NULL,
     ADD `realm` varchar(255) DEFAULT NULL;
 
+
+-- Alter for machine_account
+--
+
+ALTER TABLE node
+    ADD `machine_account` varchar(255) DEFAULT NULL;
