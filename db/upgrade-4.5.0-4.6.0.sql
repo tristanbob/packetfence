cdef9e026ce38d75af2cbe2148535ce65a5700c4
Derek Wuelfrath <dwuelfrath@inverse.ca>
2014-12-05 15:28:09 -0500

    New 'default' default user

--- db/upgrade-4.5.0-4.6.0.sql
+++ db/upgrade-4.5.0-4.6.0.sql
@@ -9,3 +9,7 @@ ALTER TABLE locationlog_history
     ADD `stripped_user_name` varchar(255) DEFAULT NULL,
     ADD `realm` varchar(255) DEFAULT NULL;
 
+-- Insert for default user
+--
+
+INSERT INTO `person` (pid,notes) VALUES ("default","Default User - do not delete");
